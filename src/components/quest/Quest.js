207c4e9e45a675b7ce3642d3ba8770e809d65ae3
Thyago Barbosa Rodrigues <thyago.rodrigues@hive.app>
2022-07-24 12:15:19 +0200

    Populates more quest fields dynamically

--- src/components/quest/Quest.js
+++ src/components/quest/Quest.js
@@ -1,6 +1,6 @@
 export default function Quest(props) {
     const { quest } = props
-    
+
     if (quest) {
         return (
             <div className="flex flex-col p-4 text-white col-span-2">
@@ -8,32 +8,25 @@ export default function Quest(props) {
                     <h1>{quest.title}</h1>
                 </div>
                 <div className="my-5">
-                    <h2 className="text-3xl font-semibold">Requirements</h2>
-                    <ul className="py-3 pl-5">
-                        <li>Ruby neophyte</li>
-                        <li>Rails neophyte</li>
-                    </ul>
-                </div>
-                <div className="my-5">
                     <h2 className="text-3xl font-semibold">Description</h2>
                     <div className="mt-2 text-justify">
-                        <p>The new guy who joins our conversation has been successful for a longer time, though. He runs a podcast I’ve heard of, and a lady next to me says “Oh I haven’t seen you since Jerry’s wedding in the south of Italy”. I ask him how he got where he is, and he says well, he actually is in real estate, and this is where he makes most of his money, and that’s how he throws these parties. Oh, this party is his party. I pry further, and he says he got into real estate after spending a few years working in finance on Wall Street.</p>
-                        <p>How do you even get into finance?</p>
+                        {quest.description}
                     </div>
                 </div>
                 <div className="my-5">
                     <h2 className="text-3xl font-semibold">Objectives</h2>
                     <ul className="py-3 pl-5">
-                        <li>Create route</li>
-                        <li>Create controller</li>
-                        <li>Write index action</li>
+                    {quest.objectives.map((objective, index) => {
+                        return <li key={index}>{ objective.title }</li>
+                    })}
                     </ul>
                 </div>
                 <div className="my-5">
                     <h2 className="text-3xl font-semibold">Rewards</h2>
                     <ul className="py-3 pl-5">
-                        <li>Ruby XP +1</li>
-                        <li>Rails XP +1</li>
+                    {quest.rewards.map((reward, index) => {
+                        return <li key={index}>{ reward.title }</li>
+                    })}
                     </ul>
                 </div>
                 <div className="flex gap-x-7">
