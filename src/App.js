363f90130de0963ef698c9c8e461f265ed4cb8bf
Thyago Barbosa Rodrigues <thyago.rodrigues@hive.app>
2022-07-24 12:54:09 +0200

    Persists simple non-encrypted object for session

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import "./App.css"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import QuestLog from "./components/QuestLog"
 import Login from "./views/Login"
 
 function App() {
 
   const [currentUser, setCurrentUser] = useState()
+  const prepareSession = (user) => {
+    setCurrentUser(user)
+    window.localStorage.setItem("session", JSON.stringify(user))
+  }
+  const isUserLoggedIn = () => {
+    let sessionUser = window.localStorage.getItem("session")
+    if (sessionUser) {
+      return JSON.parse(sessionUser)
+    } else {
+      return false
+    }
+  }
+  
+  useEffect(() => {
+    let user = isUserLoggedIn()
+    if (user) {
+      setCurrentUser(user)
+    }
+  }, [])
 
   return (
       (currentUser ? 
@@ -13,7 +32,7 @@ function App() {
           <QuestLog />
         </div>
         :
-        <Login onLogin={setCurrentUser} />
+        <Login onLogin={prepareSession} />
       )
     );
 }
